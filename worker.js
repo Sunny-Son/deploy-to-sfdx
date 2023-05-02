4472c458683d35327464c74f57cfcb37e5f228e1
Sunny-Son <insun.son@gmail.com>
2023-05-02 09:28:47 +0900

    modified:   worker.js

--- worker.js
+++ worker.js
@@ -17,8 +17,8 @@ logger.debug('I am a worker and I am up!');
 // load helpful plugins in cloud only
 let cmd='pwd';
 //sfdx msm:user:password:set -p SFPL2018 -g User -l User --json
-if(process.env.DXLOGINURL)cmd='echo y | sfdx plugins:install sfdx-msm-plugin'
-//if(process.env.DXLOGINURL)cmd='echo y | sfdx plugins:install shane-sfdx-plugins'
+//if(process.env.DXLOGINURL)cmd='echo y | sfdx plugins:install sfdx-msm-plugin'
+if(process.env.DXLOGINURL)cmd='echo y | sfdx plugins:install shane-sfdx-plugins'
 exec(cmd)
 // auth to the hub
 .then( (result) => {
