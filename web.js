5e5c9d45d5236ff9c817c5266f33305d84358ff7
Sunny-Son <insun.son@gmail.com>
2023-05-09 09:56:46 +0900

    modified:   web.js

--- web.js
+++ web.js
@@ -175,8 +175,10 @@ app.get('/trailhead', (req, res) => {
   logger.debug('+++SUNNY /trailhead username = [' + req.query.username + ']+++');
   logger.debug('+++SUNNY /trailhead password = [' + req.query.password + ']+++');
   logger.debug('+++SUNNY /trailhead instanceurl = [' + req.query.instanceurl + ']+++');
-  if(action == 'checkTravelApprovalRecord' && req.query.username != null && req.query.password != null && req.query.instanceurl != null) {
-    return sunnytrailhead.login(req.query.username,req.query.password,req.query.instanceurl,action);
+  //if(action == 'checkTravelApprovalRecord' && req.query.username != null && req.query.password != null && req.query.instanceurl != null) {
+  //  return sunnytrailhead.login(req.query.username,req.query.password,req.query.instanceurl,action);
+  if(action == 'checkTravelApprovalRecord' && req.query.username != null && req.query.password != null) {
+    return sunnytrailhead.login(req.query.username,req.query.password,action);
   }
   //return res.render('pages/trailhead', { username: req.query.username ,password:req.query.password});
 });
