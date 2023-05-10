673561ef6df913c221f38dbb516645d8786901b8
Sunny-Son <insun.son@gmail.com>
2023-05-10 13:41:25 +0900

    modified:   web.js

--- web.js
+++ web.js
@@ -186,7 +186,7 @@ app.get('/checkdashboard', (req, res) => {
   return res.render('pages/trailhead', { username: req.query.username ,password:req.query.password});
 });
 
-app.get('/trailhead',  (req, res) => {
+app.get('/trailhead', async (req, res) => {
 
   //https://sunny-deployer1.herokuapp.com/trailhead?action=check&username=sdlkfaj@example.com&password=aldkfjas
   //https://sunny-deployer1.herokuapp.com/trailhead?action=checkdata&username=sdlkfaj@example.com&password=aldkfjas
@@ -208,7 +208,8 @@ app.get('/trailhead',  (req, res) => {
   if(action == 'checkTravelApprovalRecord' && req.query.username != null && req.query.password != null) {
     logger.debug('+++ SUNNY : web.js : trailhead_checkTravelApprovalRecord');
     
-      trailhead_checkTravelApprovalRecord().then(result => res.send(result));
+      const result = await trailhead_checkTravelApprovalRecord();
+      res.send(result);
       //res.send({result: _result})
       //res.send(sunnytrailhead.trailhead_checkTravelApprovalRecord());
     
